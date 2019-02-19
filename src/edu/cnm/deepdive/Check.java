571220794da781c086d886b3e095804d2cb00ffc
Felipe Cook <felipepcook@gmail.com>
2019-02-19 12:45:53 -0700

    added comments

--- src/edu/cnm/deepdive/Check.java
+++ src/edu/cnm/deepdive/Check.java
@@ -6,10 +6,11 @@ public class Check {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a number: ");
     int a = scanner.nextInt();
     Check.digitSum(a);
-    System.out.println(a);
-    System.out.println(a / 9);
+    System.out.println(digitSum(a));
+    System.out.println(digitSum(a) / 9);
     System.out.println(digitSum(a));
     System.out.println(digitSum(a) / 9);
 
@@ -18,13 +19,20 @@ public class Check {
 
 
   public static int digitSum(int a) {
+
     String b;
+    // creates local string variable
+
     int c = 0;
+    //creates local int variable
+
     b = Integer.toString(a);
-    for (int i = 0; i < b.length(); i++) {
-      c += b.charAt(i) - 48;
+    // assigns parameter int using to string method to the local b string variable
+
+    for (int i = 0; i < b.length(); i++) { // iterates over the local b string variable
+      c += b.charAt(i) - 48; // assigns the numeric value of the individual chars to the local variable c
     }
-    return c;
+    return c; // return
 
 
   }
