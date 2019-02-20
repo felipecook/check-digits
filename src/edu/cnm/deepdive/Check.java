2f799ed947b96d34cca32dac211e46c38f58780c
Felipe Cook <felipepcook@gmail.com>
2019-02-19 20:29:06 -0700

    attempted #3 of alternate, could not complete.

--- src/edu/cnm/deepdive/Check.java
+++ src/edu/cnm/deepdive/Check.java
@@ -37,4 +37,34 @@ public class Check {
 
   }
 
+  public static int reducedDigitSum(int input) {
+
+    String stringVariable;
+
+    int varHolder = 0;
+
+    stringVariable = Integer.toString(input);
+
+    for (int i = 0; i < stringVariable.length(); i++) {
+      varHolder += stringVariable.charAt(i) - 48;
+    }
+
+    stringVariable = Integer.toString(varHolder);
+    int[] varHolderArray = new int[stringVariable.length()];
+    for (int i = 0; i < stringVariable.length() - 1; i++) {
+      varHolderArray[i] = stringVariable.charAt(i) - 48;
+    }
+
+    if (varHolder > 9) {
+      for (int i = 0; i < varHolderArray.length - 2; i++) {
+        varHolderArray[0] += varHolderArray[i];
+      }
+
+    }
+
+
+
+  }
+
+
 }
